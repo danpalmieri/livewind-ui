aa90e7689c070239965ad9c0a01efbed0536192d
Dan Palmieri <dlspalmieri@gmail.com>
2023-03-12 21:02:57 -0300

    fix icon spacing

--- resources/views/components/sidebar/item.blade.php
+++ resources/views/components/sidebar/item.blade.php
@@ -5,12 +5,12 @@
 
 <li>
     <a {{ $attributes->class([
-        'flex items-center h-10 px-2 space-x-2 rounded-lg',
+        'flex items-center h-10 px-2 space-x-3 rounded-lg',
         'transition hover:bg-gray-500/5 focus:bg-primary-500/10 focus:text-primary-600 focus:outline-none' => ! $active,
         'text-white bg-primary-600' => $active,
     ]) }}>
         @if ($icon)
-            <x-dynamic-component :component="$icon" :class="'w-6 h-6' . ($active ? '' : ' text-primary-500')" />
+            <x-dynamic-component :component="$icon" :class="'w-5 h-5' . ($active ? '' : ' text-primary-500')" />
         @endif
 
         <span class="font-medium">{{ $slot }}</span>
