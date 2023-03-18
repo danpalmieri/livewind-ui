65f3af6744e4f8e5b50f01b225283bc9c360c504
Dan Palmieri <dlspalmieri@gmail.com>
2023-03-18 19:04:51 -0300

    fix

--- resources/views/components/layouts/sidebar.blade.php
+++ resources/views/components/layouts/sidebar.blade.php
@@ -25,18 +25,18 @@
     ></button>
 
     <aside
-        x-bind:class="open ? 'translate-x-0' : '-translate-x-full duration-300'"
+        x-bind:class="open ? 'translate-x-0' : '-translate-x-full'"
         class="{{ generateClasses ([
-            'fixed inset-y-0 left-0 z-20 h-screen transition lg:z-0',
+            'fixed inset-y-0 left-0 z-20 h-screen transition lg:z-0 duration-300',
             'translate-x-0' => $open,
-            '-translate-x-full duration-300' => ! $open
+            '-translate-x-full' => ! $open
         ]) }}"
     >
         {{ $sidebar }}
     </aside>
 
     <main x-bind:class="open ? '{{ $iconMenu ? 'lg:pl-80' : 'lg:pl-72' }}' : ''" class="{{ generateClasses([
-        'flex-1 min-h-screen w-screen',
+        'flex-1 min-h-screen w-screen transition duration-300',
         'lg:pl-80' => $open && $iconMenu,
         'lg:pl-72' => $open && ! $iconMenu
     ]) }}">
