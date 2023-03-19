cf3c7bd551994bf5fdaeea6d2e9676819a64efcd
Dan Palmieri <dlspalmieri@gmail.com>
2023-03-19 20:13:06 -0300

    fix

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -13,7 +13,7 @@
         'bg-primary-600 hover:bg-primary-500 focus:bg-primary-700 focus:ring-offset-primary-700' => $color === 'primary',
         'bg-zinc-800 hover:bg-zinc-700 focus:bg-zinc-600 focus:ring-offset-zinc-600' => $color === 'dark',
         'h-9 px-4' => $size === 'md',
-        'text-white shadow focus:ring-white' => $color !== 'secondary',
+        'text-white focus:ring-white' => $color !== 'secondary',
         'bg-danger-600 hover:bg-danger-500 focus:bg-danger-700 focus:ring-offset-danger-700' => $color === 'danger',
         'text-gray-800 bg-white border hover:bg-gray-50 focus:ring-primary-600 focus:text-primary-600 focus:bg-primary-50 focus:border-primary-600' => $color === 'secondary',
         'bg-success-600 hover:bg-success-500 focus:bg-success-700 focus:ring-offset-success-700' => $color === 'success',
