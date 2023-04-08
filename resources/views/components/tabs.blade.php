2a85bee9e6237de47e1e4ee72b2c4cf219b0839b
Dan Palmieri <dlspalmieri@gmail.com>
2023-04-08 12:31:51 -0300

    fix

--- resources/views/components/tabs.blade.php
+++ resources/views/components/tabs.blade.php
@@ -1,3 +1,3 @@
-<nav {{ $attributes->class(['inline-flex items-center justify-center p-1.5 space-x-1 text-sm text-gray-600 bg-gray-500/5 rounded']) }}>
+<nav {{ $attributes->class(['inline-flex items-center justify-center space-x-1 text-sm text-gray-600']) }}>
     {{ $slot }}
 </nav>
