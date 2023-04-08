740e90126efb2209e0202d92216535aeb150e638
Dan Palmieri <dlspalmieri@gmail.com>
2023-04-08 10:22:51 -0300

    fix

--- resources/views/components/sidebar/item-group.blade.php
+++ resources/views/components/sidebar/item-group.blade.php
@@ -4,7 +4,7 @@
 
 <nav {{ $attributes->class(['my-4 space-y-2']) }}>
     @if ($heading)
-        <p class="px-4 text-lg font-semibold">{{ $heading }}</p>
+        <p class="px-4 font-medium">{{ $heading }}</p>
     @endif
 
     <ul class="px-2 space-y-1">
