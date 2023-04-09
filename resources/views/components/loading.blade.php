3ed8ede3b926037e762317346ab08e0fbeaa0842
Dan Palmieri <dlspalmieri@gmail.com>
2023-04-09 16:55:55 -0300

    fix

--- resources/views/components/loading.blade.php
+++ resources/views/components/loading.blade.php
@@ -1,5 +1,5 @@
 @props(['color' => '#000'])
-<svg class="mx-auto mb-12 animate-spin" fill="none" height="60" width="60">
+<svg class="mx-auto animate-spin" fill="none" height="60" width="60">
     <circle class="stroke-zinc-900/10" cx="30.005" cy="30.005" r="28" stroke-width="2"></circle>
     <path
         d="M16.002 54.253C2.612 46.521-1.975 29.397 5.755 16.005 13.486 2.612 30.608-1.976 43.998 5.756c13.39 7.732 17.977 24.857 10.247 38.249"
