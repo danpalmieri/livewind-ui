154dd2d0db06f0a2f26648435a0d41f0f0ccb0c1
Dan Palmieri <dlspalmieri@gmail.com>
2023-04-17 15:16:58 -0300

    fix

--- resources/views/components/sidebar/item-group.blade.php
+++ resources/views/components/sidebar/item-group.blade.php
@@ -2,7 +2,7 @@
     'heading' => null,
 ])
 
-<nav {{ $attributes->class(['my-2.5 space-y-2']) }}>
+<nav {{ $attributes->class(['py-3 space-y-2']) }}>
     @if ($heading)
         <p class="px-4 font-medium">{{ $heading }}</p>
     @endif
