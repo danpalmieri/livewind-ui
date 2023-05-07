22cc2714b2853f0b4729ab314dfea4828e9c7dc2
Dan Palmieri <dlspalmieri@gmail.com>
2023-05-07 20:21:08 -0300

    fix

--- resources/views/components/ready-state.blade.php
+++ resources/views/components/ready-state.blade.php
@@ -1,9 +1,11 @@
 <div wire:init="init">
-    @if($ready)
-    <div wire:key="ready-section">
-        {{ $slot }}
+    <div>
+        @if($ready)
+        <div wire:key="ready-section">
+            {{ $slot }}
+        </div>
+        @else
+        <x-lui::loading inline />
+        @endif
     </div>
-    @else
-    <x-lui::loading inline />
-    @endif
 </div>
