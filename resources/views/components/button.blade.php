15b852ef976525ff876a8c0fd97a2e1282892a62
Dan Palmieri <dlspalmieri@gmail.com>
2023-05-07 06:37:40 -0300

    fix

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -47,19 +47,7 @@
         @if ($icon && $iconPosition === 'before')
             @if($loadingFeedback)
             <x-dynamic-component wire:loading.remove :component="$icon" :class="$iconClasses" />
-            <svg wire:loading :class="$iconClasses . ' animate-spin'" fill="none">
-                <circle class="stroke-zinc-900/10" cx="30.005" cy="30.005" r="28" stroke-width="2"></circle>
-                <path
-                    d="M16.002 54.253C2.612 46.521-1.975 29.397 5.755 16.005 13.486 2.612 30.608-1.976 43.998 5.756c13.39 7.732 17.977 24.857 10.247 38.249"
-                    stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="url(#paint0_linear)"></path>
-                <defs>
-                        <linearGradient gradientUnits="userSpaceOnUse" id="paint0_linear" x1="50.322" x2="6.877" y1="19.8"
-                        y2="42.051">
-                        <stop stop-color="#fff"></stop>
-                        <stop offset="1" stop-color="#fff" stop-opacity="0"></stop>
-                    </linearGradient>
-                </defs>
-            </svg>
+            <x-dynamic-component wire:loading component="bi-circle-fill" class="{{ $iconClasses }} animate-pulse" />
             @else
             <x-dynamic-component :component="$icon" :class="$iconClasses" />
             @endif
