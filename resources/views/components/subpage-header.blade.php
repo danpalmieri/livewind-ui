4c24c18af9fdaf8f905b44c9693fca5490d71238
Dan Palmieri <dlspalmieri@gmail.com>
2023-05-21 09:15:11 -0300

    fix

--- resources/views/components/subpage-header.blade.php
+++ resources/views/components/subpage-header.blade.php
@@ -7,8 +7,8 @@
 
 <div class="flex justify-between items-center">
     <div>
-        <h3 class="text-xl font-semibold">{{ $title }}</h3>
-        <p class="text-zinc-500">{{ $description }}</p>
+        <h3 class="text-2xl font-semibold">{{ $title }}</h3>
+        <p class="text-zinc-600 mt-1">{{ $description }}</p>
     </div>
 
     {{ $slot }}
