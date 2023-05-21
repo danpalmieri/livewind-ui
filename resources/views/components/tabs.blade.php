6bbfedf195f51e2222c89fbfcf864b5d1e41979d
Dan Palmieri <dlspalmieri@gmail.com>
2023-05-21 07:54:38 -0300

    fix

--- resources/views/components/tabs.blade.php
+++ resources/views/components/tabs.blade.php
@@ -1,3 +1,13 @@
-<nav {{ $attributes->class(['inline-flex items-center justify-center space-x-3 text-sm text-gray-600']) }}>
+@props([
+    'size' => 'lg',
+])
+@php
+    $classes = generateClasses([
+        'inline-flex items-center justify-center space-x-3 text-gray-600',
+        'text-lg' => $size === 'lg',
+        'text-sm' => $size === 'sm',
+    ]);
+@endphp
+<nav {{ $attributes->class([$classes]) }}>
     {{ $slot }}
 </nav>
