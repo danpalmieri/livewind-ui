67b76b3bb05d388f5f23ab2d3afb251de897585b
Dan Palmieri <dlspalmieri@gmail.com>
2023-05-21 11:58:26 -0300

    fix

--- resources/views/components/page.blade.php
+++ resources/views/components/page.blade.php
@@ -1,5 +1,5 @@
 <div class="px-4 pb-12 lg:px-0">
-    <div class="mx-auto max-w-7xl space-y-7 sm:px-6 lg:px-8">
+    <div class="mx-auto max-w-7xl space-y-9 sm:px-6 lg:px-8">
         {{ $slot }}
     </div>
 </div>
