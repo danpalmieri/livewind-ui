ce5a834a6368ce96364e553b661bc014263b9c2f
Dan Palmieri <dlspalmieri@gmail.com>
2023-05-22 12:27:41 -0300

    fix

--- resources/views/components/sidebar/item.blade.php
+++ resources/views/components/sidebar/item.blade.php
@@ -16,8 +16,8 @@
             <x-dynamic-component :component="$icon" {{ $attributes->class([
                 'w-[1.15rem] h-[1.15rem]',
                 'text-primary-800' => $backgroundColor === 'light' && $active,
-                'text-primary-500' => $backgroundColor === 'dark' && $active,
-                'text-primary-500' => $backgroundColor === 'light' && ! $active,
+                'text-primary-600 animate-pulse' => $backgroundColor === 'dark' && $active,
+                'text-primary-600' => $backgroundColor === 'light' && ! $active,
                 'text-white/90' => $backgroundColor === 'dark' && ! $active,
             ]) }} />
         @endif
@@ -29,7 +29,7 @@
                 <x-icon name="bi-arrow-left-circle-fill" {{ $attributes->class([
                     'rounded-full w-2 h-2',
                     'text-primary-800 bg-primary-800' => $backgroundColor === 'light',
-                    'text-primary-500 bg-primary-500' => $backgroundColor === 'dark',
+                    'text-primary-600 bg-primary-600' => $backgroundColor === 'dark',
                 ]) }} />
             </div>
         @endif
