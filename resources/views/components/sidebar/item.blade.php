2bef2a2004f2cdec6f7c5feb925a02c027c17f1d
Dan Palmieri <dlspalmieri@gmail.com>
2023-05-22 10:47:29 -0300

    fix

--- resources/views/components/sidebar/item.blade.php
+++ resources/views/components/sidebar/item.blade.php
@@ -8,15 +8,16 @@
     <a {{ $attributes->class([
         'flex transition items-center focus:outline-none h-10 px-4 space-x-5 rounded',
         'text-white/90 hover:bg-white/10' => $backgroundColor === 'dark' && ! $active,
-        'text-white font-bold focus:bg-white/80 focus:text-white' => $backgroundColor === 'dark' && $active,
-        'hover:bg-zinc-500/5' => $backgroundColor === 'light' && ! $active,
-        'hover:bg-zinc-500/5 font-bold focus:bg-primary-500/10 focus:text-primary-600 ' => $backgroundColor === 'light' && $active,
+        'text-primary-500 font-bold focus:bg-primary-500/80 focus:text-primary-500' => $backgroundColor === 'dark' && $active,
+        'hover:bg-gray-500/5' => $backgroundColor === 'light' && ! $active,
+        'hover:bg-gray-500/5 font-bold focus:bg-primary-500/10 focus:text-primary-600 ' => $backgroundColor === 'light' && $active,
     ]) }}>
         @if ($icon)
             <x-dynamic-component :component="$icon" {{ $attributes->class([
                 'w-[1.15rem] h-[1.15rem]',
                 'text-primary-800' => $backgroundColor === 'light' && $active,
-                'text-primary-600' => $active,
+                'text-primary-500' => $backgroundColor === 'dark' && $active,
+                'text-primary-500' => $backgroundColor === 'light' && ! $active,
                 'text-white/90' => $backgroundColor === 'dark' && ! $active,
             ]) }} />
         @endif
@@ -28,7 +29,7 @@
                 <x-icon name="bi-arrow-left-circle-fill" {{ $attributes->class([
                     'rounded-full w-2 h-2',
                     'text-primary-800 bg-primary-800' => $backgroundColor === 'light',
-                    'text-white bg-white' => $backgroundColor === 'dark',
+                    'text-primary-500 bg-primary-500' => $backgroundColor === 'dark',
                 ]) }} />
             </div>
         @endif
