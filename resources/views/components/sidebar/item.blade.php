5550c56642defe772465568285fc3d36af3bcd8d
Dan Palmieri <dlspalmieri@gmail.com>
2023-05-30 11:35:57 -0300

    fix

--- resources/views/components/sidebar/item.blade.php
+++ resources/views/components/sidebar/item.blade.php
@@ -7,15 +7,16 @@
 <li>
     <a {{ $attributes->class([
         'flex transition items-center focus:outline-none h-10 px-4 space-x-5 rounded',
+        'font-semibold' => $active,
         'text-white/90 hover:bg-white/10' => $backgroundColor === 'dark' && ! $active,
-        'text-primary-600 font-semibold focus:bg-primary-600/80 bg-white focus:text-primary-600' => $backgroundColor === 'dark' && $active,
+        'text-primary-600 focus:bg-primary-600/80 bg-white focus:text-primary-600' => $backgroundColor === 'dark' && $active,
         'hover:bg-gray-500/5' => $backgroundColor === 'light' && ! $active,
-        'hover:bg-gray-500/5 font-bold focus:bg-primary-500/10 focus:text-primary-600 ' => $backgroundColor === 'light' && $active,
+        'hover:bg-gray-500/5 focus:bg-primary-500/10 focus:text-primary-600 ' => $backgroundColor === 'light' && $active,
     ]) }}>
         @if ($icon)
             <x-dynamic-component :component="$icon" {{ $attributes->class([
                 'w-[1.15rem] h-[1.15rem]',
-                'text-primary-800' => $backgroundColor === 'light' && $active,
+                'text-black' => $backgroundColor === 'light' && $active,
                 'text-primary-600' => $backgroundColor === 'dark' && $active,
                 'text-primary-600' => $backgroundColor === 'light' && ! $active,
                 'text-white/90' => $backgroundColor === 'dark' && ! $active,
@@ -28,7 +29,7 @@
             <div class="flex-1 flex justify-end">
                 <x-icon name="bi-arrow-left-circle-fill" {{ $attributes->class([
                     'rounded-full w-2 h-2 animate-pulse',
-                    'text-primary-800 bg-primary-800' => $backgroundColor === 'light',
+                    'text-black bg-black' => $backgroundColor === 'light',
                     'text-primary-600 bg-primary-600' => $backgroundColor === 'dark',
                 ]) }} />
             </div>
