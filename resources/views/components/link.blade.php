245bae1ab2072ebf4e315a98e8caabc7555043da
Dan Palmieri <dlspalmieri@gmail.com>
2023-05-30 16:27:33 -0300

    fix

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -8,6 +8,7 @@
     $linkClasses = generateClasses([
         'transition focus:outline-none focus:underline',
         'text-primary-600 hover:text-primary-600' => $color === 'primary',
+        'text-zinc-900 hover:text-zinc-900' => $color === 'dark',
         'text-danger-600 hover:text-danger-500' => $color === 'danger',
         'text-zinc-600 hover:text-zinc-500' => $color === 'secondary',
         'text-success-600 hover:text-success-500' => $color === 'success',
