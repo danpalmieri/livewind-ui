16fd56ac5b3d50c09b7d710dc2f273ad93fe36cf
Dan Palmieri <dlspalmieri@gmail.com>
2023-10-17 10:57:36 -0300

    fix

--- resources/views/components/column-section.blade.php
+++ resources/views/components/column-section.blade.php
@@ -20,7 +20,7 @@
         </div>
 
         @isset($actions)
-        <div class="flex items-center justify-end bg-zinc-100 px-4 py-3 text-right sm:px-6">
+        <div class="flex items-center justify-end bg-zinc-200/70 px-4 py-3 text-right sm:px-6">
             {{ $actions }}
         </div>
         @endisset
