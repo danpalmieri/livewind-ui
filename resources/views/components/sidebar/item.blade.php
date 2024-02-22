18556460017aca42bcd9fd8cce499d77ee85ff6a
Daniel Palmieri <dlspalmieri@gmail.com>
2024-02-22 11:55:55 -0300

    update

--- resources/views/components/sidebar/item.blade.php
+++ resources/views/components/sidebar/item.blade.php
@@ -11,14 +11,14 @@
         'text-white/90 hover:bg-white/10' => $backgroundColor === 'dark' && ! $active,
         'text-black focus:bg-black/80 bg-white focus:text-black' => $backgroundColor === 'dark' && $active,
         'hover:bg-gray-500/5' => $backgroundColor === 'light' && ! $active,
-        'hover:bg-gray-500/5 focus:bg-primary-500/10 focus:text-primary-600 ' => $backgroundColor === 'light' && $active,
+        'hover:bg-gray-500/5 focus:bg-gray-500/10 focus:text-gray-600 ' => $backgroundColor === 'light' && $active,
     ]) }}>
         @if ($icon)
             <x-dynamic-component :component="$icon" {{ $attributes->class([
                 'w-[.95rem] h-[.95rem]',
                 'text-black' => $backgroundColor === 'light' && $active,
                 'text-black' => $backgroundColor === 'dark' && $active,
-                'text-primary-600' => $backgroundColor === 'light' && ! $active,
+                'text-gray-600' => $backgroundColor === 'light' && ! $active,
                 'text-white/90' => $backgroundColor === 'dark' && ! $active,
             ]) }} />
         @endif
@@ -30,7 +30,7 @@
 {{--                <x-icon name="bi-arrow-left-circle-fill" {{ $attributes->class([--}}
 {{--                    'rounded-full w-2 h-2',--}}
 {{--                    'text-black bg-black' => $backgroundColor === 'light',--}}
-{{--                    'text-primary-600 bg-primary-600' => $backgroundColor === 'dark',--}}
+{{--                    'text-gray-600 bg-gray-600' => $backgroundColor === 'dark',--}}
 {{--                ]) }} />--}}
 {{--            </div>--}}
 {{--        @endif--}}
