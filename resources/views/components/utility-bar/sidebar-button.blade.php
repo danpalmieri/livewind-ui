9fff5f9830ce840cb1748a0834676b1ce6e34ad8
Daniel Palmieri <dlspalmieri@gmail.com>
2024-02-22 00:06:26 -0300

    update

--- resources/views/components/utility-bar/sidebar-button.blade.php
+++ resources/views/components/utility-bar/sidebar-button.blade.php
@@ -3,6 +3,6 @@
     'alwaysVisible' => false
 ])
 
-<div {{ $attributes->class([($alwaysVisible ? '' : 'lg:hidden').' mr-2 -ml-2']) }}>
+<div {{ $attributes->class([($alwaysVisible ? '' : 'lg:hidden').' ']) }}>
     <x-lui::icon-button x-on:click="open = !open" label="Open menu" :icon="$icon" />
 </div>
