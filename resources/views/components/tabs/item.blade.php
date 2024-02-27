b6c31d8cde27aec0241dd5129f2576613628d226
Daniel Palmieri <dlspalmieri@gmail.com>
2024-02-27 11:54:37 -0300

    update

--- resources/views/components/tabs/item.blade.php
+++ resources/views/components/tabs/item.blade.php
@@ -8,7 +8,7 @@
 @php
     $buttonClasses = generateClasses([
         'flex font-medium items-center h-6 items-center border-b relative top-[7px] space-x-2 pb-[20px] transition focus:outline-none',
-        'text-gray-600 border-transparent' => ! $active,
+        'text-gray-600 border-transparent hover:text-gray-900' => ! $active,
         'text-gray-900 border-black' => $active,
     ]);
 @endphp
@@ -19,14 +19,14 @@
             {{ $attributes->class([$buttonClasses]) }}
     >
         @if ($icon)
-            <x-dynamic-component :component="$icon" class="w-5 h-5 opacity-80" />
+            <x-dynamic-component :component="$icon" class="w-4 h-4 opacity-80" />
         @endif
         <span>{{ $slot }}</span>
     </button>
 @elseif ($tag === 'a')
     <a {{ $attributes->class([$buttonClasses]) }}>
         @if ($icon)
-            <x-dynamic-component :component="$icon" class="w-5 h-5 opacity-80" />
+            <x-dynamic-component :component="$icon" class="w-4 h-4 opacity-80" />
         @endif
         <span>{{ $slot }}</span>
     </a>
