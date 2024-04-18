b82b45ba297293797a83afde29b7a59aaffb90b3
Daniel Palmieri <dlspalmieri@gmail.com>
2024-04-18 10:41:37 -0300

    update

--- resources/views/components/sidebar/item-group.blade.php
+++ resources/views/components/sidebar/item-group.blade.php
@@ -2,16 +2,19 @@
     'heading' => null,
     'action' => null,
     'actionLabel' => null,
+    'collapsible' => true
 ])
 
 <nav x-data="{open: true}" {{ $attributes->class(['py-2 space-y-1']) }}>
     @if ($heading)
         <div class="flex px-2 justify-between">
-            <button class="px-1.5 py-1 rounded hover:bg-gray-100 flex text-xs space-x-1 text-gray-500 font-medium" @click="open = !open" >
+            <button class="px-1.5 py-1 rounded @if($collapsible) hover:bg-gray-100 @else cursor-default @endif flex text-xs space-x-1 text-gray-500 font-medium" @if($collapsible) @click="open = !open" @endif>
+                @if($collapsible)
                 <span class="w-3 h-3 inline-block">
                     <x-icon name="bi-chevron-down" class="w-3 h-3 inline-block" x-show="open" />
                     <x-icon name="bi-chevron-right" class="w-3 h-3 inline-block" x-show="!open" />
                 </span>
+                @endif
                 <span>{{ $heading }}</span>
             </button>
             @if($action)
