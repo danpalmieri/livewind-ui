d8f2f7ea2b033261396251795e2477c73e468fb5
Daniel Palmieri <dlspalmieri@gmail.com>
2024-09-18 10:59:34 -0300

    update

--- resources/views/components/sidebar/item.blade.php
+++ resources/views/components/sidebar/item.blade.php
@@ -7,7 +7,7 @@
 <li>
     <a {{ $attributes->class([
         'flex transition items-center focus:outline-none h-8 px-2 tracking-tight text-gray-800 space-x-3 rounded text-[14px]',
-        'bg-gray-100' => $active,
+        'text-primary-600 font-semibold' => $active,
         'text-white/90 hover:bg-white/10' => $backgroundColor === 'dark' && ! $active,
         'text-black focus:bg-black/80 bg-white focus:text-black' => $backgroundColor === 'dark' && $active,
         'hover:bg-gray-500/5' => $backgroundColor === 'light' && ! $active,
@@ -24,15 +24,5 @@
         @endif
 
         <span class="tracking-normal">{{ $slot }}</span>
-
-{{--        @if($active)--}}
-{{--            <div class="flex-1 flex justify-end">--}}
-{{--                <x-icon name="bi-arrow-left-circle-fill" {{ $attributes->class([--}}
-{{--                    'rounded-full w-2 h-2',--}}
-{{--                    'text-black bg-black' => $backgroundColor === 'light',--}}
-{{--                    'text-gray-600 bg-gray-600' => $backgroundColor === 'dark',--}}
-{{--                ]) }} />--}}
-{{--            </div>--}}
-{{--        @endif--}}
     </a>
 </li>
