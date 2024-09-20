534b5e8e328209a141c29ada40f0a7880f302868
Daniel Palmieri <dlspalmieri@gmail.com>
2024-09-20 11:21:54 -0300

    change borders

--- resources/views/components/tabs/item.blade.php
+++ resources/views/components/tabs/item.blade.php
@@ -7,7 +7,7 @@
 
 @php
     $buttonClasses = generateClasses([
-        'flex font-medium items-center h-7 items-center border-b relative top-[7px] space-x-2 pb-[20px] transition focus:outline-none',
+        'flex font-medium items-center h-7 items-center border-b-2 relative top-[7px] space-x-2 pb-[20px] transition focus:outline-none',
         'text-gray-600 border-transparent hover:text-gray-900' => ! $active,
         'text-primary-700 font-semibold border-primary-700' => $active,
     ]);
@@ -28,6 +28,6 @@
         @if ($icon)
             <x-dynamic-component :component="$icon" class="w-4 h-4 opacity-80" />
         @endif
-        <span @if($active) class="border block rounded-lg border-gray-200/70 px-2 py-1" @endif>{{ $slot }}</span>
+        <span @if($active) class="block rounded-lg border-gray-200/70 py-1" @endif>{{ $slot }}</span>
     </a>
 @endif
